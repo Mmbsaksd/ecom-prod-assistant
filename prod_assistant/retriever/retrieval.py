2fae4b9ba6e96d6dbe2df844c95463039ba0c265
mmbsaksd <bs.munavar@gmail.com>
2025-09-19 05:40:47 +0530

    retrival skelton updated

---
+++ prod_assistant/retriever/retrieval.py
@@ -0,0 +1,20 @@
+import os
+from langchain_astradb import AstraDBByteStore
+from typing import List
+from langchain_core.documents import Document
+from utils.config_loader import load_config
+from utils.model_loader import ModelLoader
+from dotenv import load_dotenv
+
+class Retriever:
+    def __init__(self):
+        pass
+
+    def load_env_variables(self):
+        pass
+
+    def load_retriever(self):
+        pass
+
+    def call_retriever(self):
+        pass
