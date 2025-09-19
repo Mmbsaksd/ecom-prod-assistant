f8a1b864c39228c71aa2377280a6f6ef492353aa
mmbsaksd <bs.munavar@gmail.com>
2025-09-19 08:58:17 +0530

    retriver is working fine

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -1,20 +1,59 @@
 import os
-from langchain_astradb import AstraDBByteStore
+from langchain_astradb import AstraDBVectorStore
 from typing import List
 from langchain_core.documents import Document
-from utils.config_loader import load_config
-from utils.model_loader import ModelLoader
+from prod_assistant.utils.config_loader import load_config
+from prod_assistant.utils.model_loader import ModelLoader
 from dotenv import load_dotenv
 
 class Retriever:
     def __init__(self):
-        pass
+        self.model_loader = ModelLoader()
+        self.config = load_config()
+        self.load_env_variables()
+        self.vstore = None
+        self.retriever = None
 
     def load_env_variables(self):
-        pass
+        load_dotenv()
+
+        required_vars = ["GOOGLE_API_KEY", "ASTRA_DB_API_ENDPOINT","ASTRA_DB_APPLICATION_TOKEN","ASTRA_DB_KEYSPACE"]
+        missing_vars = [var for var in required_vars if os.getenv(var) is None]
+
+        if missing_vars:
+            raise EnvironmentError(f"Missing enviroment variables: {missing_vars}")
+        
+        self.google_api_key = os.getenv("GOOGLE_API_KEY")
+        self.db_api_endpoint = os.getenv("ASTRA_DB_API_ENDPOINT")
+        self.db_application_token = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+        self.db_keyspace = os.getenv("ASTRA_DB_KEYSPACE")
 
     def load_retriever(self):
-        pass
+        if not self.vstore:
+            collection_name = self.config["astra_db"]["collection_name"]
 
-    def call_retriever(self):
-        pass
+            self.vstore = AstraDBVectorStore(
+                embedding=self.model_loader.load_embeddings(),
+                collection_name=collection_name,
+                api_endpoint=self.db_api_endpoint,
+                token=self.db_application_token,
+                namespace=self.db_keyspace
+            )
+        if not self.retriever:
+            top_k = self.config.get("retriever", {}).get("top_k", 3)
+            retriever = self.vstore.as_retriever(search_kwargs={"k":top_k})
+            print("Retriver loaded successfully")
+            return retriever
+
+    def call_retriever(self,query):
+        retriever = self.load_retriever()
+        output = retriever.invoke(query)
+        return output
+
+if __name__=='__main__':
+    retriver_obj = Retriever()
+    user_query = "Can you suggest good budget laptop?"
+    result = retriver_obj.call_retriever(user_query)
+
+    for idx, doc in enumerate(result,1):
+        print(f"Result {idx}: {doc.page_content}\n Metadata: {doc.metadata}\n")
