a56280cb6b9784c833a6544cf853d645fcc0cd4d
mmbsaksd <bs.munavar@gmail.com>
2025-09-25 20:05:22 +0530

    updated with ai assistant part retriever

--- prod_assistant/workflow/agentic_rag_workflow.py
+++ prod_assistant/workflow/agentic_rag_workflow.py
@@ -16,5 +16,31 @@ class AgenticRAG:
     class AgentState(TypedDict):
         message: Annotated[Sequence[BaseMessage],add_messages]
 
-        def __init__(self):
-            self.retriever_obj = Retriever()
+    def __init__(self):
+        self.retriever_obj = Retriever()
+        self.model_loader = ModelLoader()
+        self.llm = self.model_loader.load_llm()
+        self.checkpointer = MemorySaver()
+        self.workflow = self._build_workflow()
+
+    def _ai_assistant(self, state:AgentState):
+        print("---CALL ASSISTANT---")
+        messages = state["messages"]
+        last_message = messages[-1].content
+
+        if any(word in last_message.lower() for word in ["price","review","product"]):
+            return {"messages":[HumanMessage(content="TOOL: retriever")]}
+        
+        prompt = ChatPromptTemplate.from_template(
+            "You are helpful assistant. Answer user directly. \n\nQuestion: {question}\nAnswer:"
+        )
+        chain = prompt| self.llm| StrOutputParser()
+        response = chain.invoke({"question": last_message})
+        return {"messages": [HumanMessage(content=response)]}
+
+    
+
+
+    def _build_workflow(self):
+        workflow = StateGraph(self.AgentState)
+        workflow.add_node("Assistant", self._ai_assistant)
