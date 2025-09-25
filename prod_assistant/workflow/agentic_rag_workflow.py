6e0360c46f7dde17e7655b8624a444379554c774
mmbsaksd <bs.munavar@gmail.com>
2025-09-25 21:22:52 +0530

    updated code with node part

--- prod_assistant/workflow/agentic_rag_workflow.py
+++ prod_assistant/workflow/agentic_rag_workflow.py
@@ -23,6 +23,21 @@ class AgenticRAG:
         self.checkpointer = MemorySaver()
         self.workflow = self._build_workflow()
 
+    def format_docs(self, docs)->str:
+        if not docs:
+            return "No relevant documents found"
+        formatted_chunks = []
+        for d in docs:
+            meta = d.metadata or {}
+            formatted = (
+                f"Title:{meta.get('product_title','N/A')}\n"
+                f"Price:{meta.get('price','N/A')}\n"
+                f"Rating:{meta.get('rating','N/A')}\n"
+                f"Review:\n{d.page_content.strip()}"
+            )
+            formatted_chunks.append(formatted)
+        return "\n\n---\n\n".join(formatted_chunks)
+
     def _ai_assistant(self, state:AgentState):
         print("---CALL ASSISTANT---")
         messages = state["messages"]
@@ -37,10 +52,38 @@ class AgenticRAG:
         chain = prompt| self.llm| StrOutputParser()
         response = chain.invoke({"question": last_message})
         return {"messages": [HumanMessage(content=response)]}
-
     
+    def _vector_retriever(self, state: AgentState):
+        print("---RETRIEVER---")
+        query = state["messages"][-1].content
+        retriever = self.retriever_obj.load_retriever()
+        docs = retriever.invoke(query)
+        context = self.format_docs(docs)
+        return {"messages":[HumanMessage(content=context)]}
+
+    def _generate(self, state: AgentState):
+        print("---GENERATE---")
+        question = state["messages"][0].content
+        docs = state["messages"][-1].content
+        prompt = ChatPromptTemplate.from_messages(
+            PROMPT_REGISTRY[PromptType.PRODUCT_BOT].template
+        )
+        chain = prompt | self.llm | StrOutputParser()
+        response = chain.invoke({"context":docs, "question":question})
+        return {"messages":[HumanMessage(content=response)]}
+    
+    def _rewrite(self, state:AgentState):
+        print("--- REWRITE --")
+        question = state["messages"][0].content
+        new_q = self.llm.invoke(
+            [HumanMessage(content=f"Rewrite the query to be more clear: {question}")]
+        )
+        return {"messages":[HumanMessage(content=new_q.content)]}
 
 
     def _build_workflow(self):
         workflow = StateGraph(self.AgentState)
         workflow.add_node("Assistant", self._ai_assistant)
+        workflow.add_node("Retriever", self._vector_retriever)
+        workflow.add_node("Generator", self._generate)
+        workflow.add_node("Rewrite")
